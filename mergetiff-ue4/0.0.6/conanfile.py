cec1e819340879fc3f2c2b2ab34a3fdb8aafe9b1
Oleksii Dovgodko <alexd.admin@gmail.com>
2024-12-11 12:23:06 +0100

    Revert "mergetiff v.0.0.19"
    This reverts commit 7d12f9cc70d36eaa0db30169746206a8387f85d6.

--- mergetiff-ue4/0.0.6/conanfile.py
+++ mergetiff-ue4/0.0.6/conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, CMake, tools
 
 class MergetiffUe4Conan(ConanFile):
     name = "mergetiff-ue4"
-    version = "0.0.19"
+    version = "0.0.6"
     license = "MIT"
     url = "https://github.com/adamrehn/ue4-conan-recipes/mergetiff-ue4"
     description = "mergetiff header-only package for Unreal Engine 4"
@@ -14,14 +14,9 @@ class MergetiffUe4Conan(ConanFile):
     def source(self):
         self.run("git clone --progress --depth=1 https://github.com/adamrehn/mergetiff-cxx.git -b v{}".format(self.version))
     
-    def requirements(self):
-        self._requireUnreal("gdal-ue4/3.10.0@adamrehn/{}".format(self.channel))
-    
     def build(self):
         
         # Install the mergetiff headers
         cmake = CMake(self)
-        cmake.configure(source_folder="mergetiff-cxx", args=[
-            "-DHEADER_ONLY=ON",
-            "-DGDAL_DIR={}".format(self.deps_cpp_info["gdal-ue4"].rootpath)])
+        cmake.configure(source_folder="mergetiff-cxx", args=["-DHEADER_ONLY=ON"])
         cmake.install()
